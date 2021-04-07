a70cfba066c65c0bb1dbf1b7c7abc2beb8430480
Andrew Friberg <andrew_friberg@brown.edu>
2021-04-07 19:53:48 -0400

    I'm going to try and reformat the code so that standardization happens in the __main__ module rather than in the methods. Fingers crossed

--- housing.py
+++ housing.py
@@ -4,6 +4,9 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
+info_types = ["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT", "MEDV"]
+
+
 # Used to plot individual features to see what impacts MEDV most directly
 def make_plot():
     med_value = np.array(data["MEDV"])
@@ -55,7 +58,7 @@ def test_add_powers():
 
 
 # Finds the training and generalization error for some data, at each split
-def in_out_error(data, splits):
+def in_out_error(train, test, power=1, show=True, printout=False):
     # Keeps track of out in and out error for different sizes of training data
     mean_sq_err = np.zeros([3, len(splits)])
     for i, number in enumerate(splits):
@@ -65,10 +68,14 @@ def in_out_error(data, splits):
         train_lab = train[:, -1]
         test_feats = test[:, :-1]
         test_lab = test[:, -1]
+
+        # Increasing the power of these polynomials
+        design = add_powers(design, power)
+        test_feats = add_powers(test_feats, power)
+
         # the transpose and inverse
         design_t = np.matrix.transpose(design)
         square = np.matmul(design_t, design)
-        print(square)
         square_i = np.linalg.inv(square)
         coeff = np.matmul(square_i, design_t)
         weights = np.matmul(coeff, train_lab)
@@ -79,9 +86,57 @@ def in_out_error(data, splits):
         mean_sq_err[0, i] = number
         mean_sq_err[1, i] = in_err
         mean_sq_err[2, i] = out_err
-    plt.scatter(mean_sq_err[0], np.log(mean_sq_err[1]), label="log-training error")
-    plt.scatter(mean_sq_err[0], np.log(mean_sq_err[2]), label="log-testing error")
-    plt.legend()
+        # Printing out the weights, if specified
+        if printout:
+            print("\nWeights for values (working with " + str(number) + " data points)")
+            for j in range(len(info_types) - 1):
+                print(str(info_types[j]) + " has weight " + str(weights[j]))
+                for pow in range(2, power + 1):
+                    idx = j + (len(info_types) - 1) * (pow - 1)
+                    print(str(info_types[j]) + "^" + str(pow) + " has weight " + str(weights[idx]))
+    if show:
+        plt.scatter(mean_sq_err[0], np.log(mean_sq_err[1]), label="log-training error")
+        plt.scatter(mean_sq_err[0], np.log(mean_sq_err[2]), label="log-testing error")
+        plt.legend()
+        plt.ylabel("ln(Error)")
+        plt.xlabel("Quantity of training data")
+        plt.show()
+    return mean_sq_err
+
+
+# Runs the in_out_error function for multiple dimensions, and graphs the result
+def in_out_multi(data, splits, powers):
+    fig, (ax1, ax2) = plt.subplots(1, 2, sharey=False)
+    for i, power in enumerate(powers):
+        mean_sq_err = in_out_error(data, splits, power=power, show=False)
+        # Plotting the data
+        alpha = 1
+        if i < len(powers) / 2:
+            alpha = max(0.1, -0.4 * (i + 1) + 1.4)
+            alpha = min(1, alpha)
+            ax1.scatter(mean_sq_err[0], np.log(mean_sq_err[1]), label="degree: " + str(power), alpha=alpha, color='g')
+            ax2.scatter(mean_sq_err[0], np.log(mean_sq_err[2]), label="degree: " + str(power), alpha=alpha, color='r')
+            ax1.plot(mean_sq_err[0], np.log(mean_sq_err[1]), alpha=alpha / 2, color='g')
+            ax2.plot(mean_sq_err[0], np.log(mean_sq_err[2]), alpha=alpha / 2, color='r')
+            # ax1.scatter(mean_sq_err[0, -2:], np.log(mean_sq_err[1, -2:]), label="degree: " + str(power), alpha=alpha, color='g')
+            # ax2.scatter(mean_sq_err[0, -2:], np.log(mean_sq_err[2, -2:]), label="degree: " + str(power), alpha=alpha, color='r')
+            # ax1.plot(mean_sq_err[0, -2:], np.log(mean_sq_err[1, -2:]), alpha=alpha / 2, color='g')
+            # ax2.plot(mean_sq_err[0, -2:], np.log(mean_sq_err[2, -2:]), alpha=alpha / 2, color='r')
+        else:
+            alpha = min(1, 0.4 * (i + 1) - 1.4)
+            alpha = max(0.1, alpha)
+            ax1.scatter(mean_sq_err[0], np.log(mean_sq_err[1]), label="degree: " + str(power), alpha=alpha, color='b')
+            ax2.scatter(mean_sq_err[0], np.log(mean_sq_err[2]), label="degree: " + str(power), alpha=alpha, color='purple')
+            ax1.plot(mean_sq_err[0], np.log(mean_sq_err[1]), alpha=alpha / 2, color='b')
+            ax2.plot(mean_sq_err[0], np.log(mean_sq_err[2]), alpha=alpha / 2, color='purple')
+            # ax1.scatter(mean_sq_err[0, -2:], np.log(mean_sq_err[1, -2:]), label="degree: " + str(power), alpha=alpha, color='b')
+            # ax2.scatter(mean_sq_err[0, -2:], np.log(mean_sq_err[2, -2:]), label="degree: " + str(power), alpha=alpha, color='purple')
+            # ax1.plot(mean_sq_err[0, -2:], np.log(mean_sq_err[1, -2:]), alpha=alpha / 2, color='b')
+            # ax2.plot(mean_sq_err[0, -2:], np.log(mean_sq_err[2, -2:]), alpha=alpha / 2, color='purple')
+    ax1.legend()
+    ax2.legend()
+    ax1.set_title("log-training error")
+    ax2.set_title("log-testing error")
     plt.ylabel("ln(Error)")
     plt.xlabel("Quantity of training data")
     plt.show()
@@ -91,14 +146,12 @@ if __name__ == "__main__":
     # Gathering data
     read = pd.read_table("housing.dat", sep="\s+")
     data = np.array(read)
-    # TODO: shuffle data once you're done debugging
-    # np.random.shuffle(data)
+    np.random.shuffle(data)
 
     # One dimensional case
     splits = [25, 50, 75, 100, 150, 200, 300]
-    # splits = [25]
-    # in_out_error(data, splits)
+    in_out_error(data, splits, power=2, show=True, printout=True)
 
     # Testing higher order polynomials
-    mod_data = add_powers(data, 3)
-    in_out_error(mod_data, splits)
+    powers = np.arange(1, 7)
+    in_out_multi(data, splits, powers)
